13ec7539564d353989ddd7f5594f9d5e96c59724
macgarcia <marquim.iron@gmail.com>
2023-07-11 00:29:01 -0300

    Functionality: Struct modification for tests.  - Modification on controller to test of login success and login without success.
    Marcos Garcia.

--- src/main/java/br/com/github/apihemofilia/apihemofilia/controllers/AuthenticationController.java
+++ src/main/java/br/com/github/apihemofilia/apihemofilia/controllers/AuthenticationController.java
@@ -1,44 +1,53 @@
 package br.com.github.apihemofilia.apihemofilia.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.github.apihemofilia.apihemofilia.domain.dtos.AuthRequest;
 import br.com.github.apihemofilia.apihemofilia.domain.dtos.AuthResponse;
 import br.com.github.apihemofilia.apihemofilia.domain.entitys.Login;
 import br.com.github.apihemofilia.apihemofilia.infra.TokenService;
-import br.com.github.apihemofilia.apihemofilia.domain.dtos.AuthRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
-@RequestMapping("/auth") 
+@RequestMapping("/auth")
 @Tag(name = "Controller to authentic any user")
 public class AuthenticationController {
-	
+
 	@Autowired
 	private AuthenticationManager authManager;
-	
+
 	@Autowired
 	private TokenService tokenService;
 
 	@Operation(summary = "Authentication", method = "POST")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "User exits on application")
-	})
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "User exits on application"),
+			@ApiResponse(responseCode = "403", description = "User not exists on application") })
 	@PostMapping(path = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<AuthResponse> login(@RequestBody final AuthRequest request) {
-		var usernamePassword = new UsernamePasswordAuthenticationToken(request.login(), request.password());
-		var auth = authManager.authenticate(usernamePassword);
-		var token = tokenService.generatedToken((Login) auth.getPrincipal());
-		return ResponseEntity.ok(new AuthResponse(token));
+		try {
+			var usernamePassword = new UsernamePasswordAuthenticationToken(request.login(), request.password());
+			var auth = authManager.authenticate(usernamePassword);
+			var token = tokenService.generatedToken((Login) auth.getPrincipal());
+			return ResponseEntity.ok(new AuthResponse(token));
+		} catch (BadCredentialsException e) {
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+		} catch (AuthenticationException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+
 	}
 }
